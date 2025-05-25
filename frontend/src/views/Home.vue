8701af6a34f5dae7673a8c280eb97cca1e6a1187
Rajeshwar Singh <rajeshwars600@gmail.com>
2025-05-25 12:30:51 -0600

    Added more config

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,3 +1,25 @@
+<script setup lang="ts">
+import MovieCard from '~/components/MovieCard.vue';
+import { VCard, VBtn } from 'vuetify/components';
+
+</script>
+
 <template>
-    <div>Home Page</div>
+    <VCard
+        class="mx-auto"
+        color="surface-variant"
+        image="https://cdn.vuetifyjs.com/docs/images/cards/dark-beach.jpg"
+        max-width="340"
+        subtitle="Take a walk down the beach"
+        title="Evening sunset">
+        <template v-slot:actions>
+            <v-btn
+                append-icon="mdi-chevron-right"
+                color="red-lighten-2"
+                text="Book Activity"
+                variant="outlined"
+                block
+            />
+        </template>
+    </VCard>
 </template>
